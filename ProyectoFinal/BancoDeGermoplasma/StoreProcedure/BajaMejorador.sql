b74f3996683596361a372d045d39abc0bc96fb10
Juan1605 <juanda_2007@hotmail.es>
2022-07-30 20:16:50 -0300

    se agregan sp de mejorador

---
+++ ProyectoFinal/BancoDeGermoplasma/StoreProcedure/BajaMejorador.sql
@@ -0,0 +1,13 @@
+﻿CREATE PROCEDURE [dbo].[BajaMejorador] 
+	@IdMejorador INT
+
+AS
+BEGIN
+	BEGIN TRY
+		UPDATE Mejoradores SET activo = 0 WHERE idMejorador = @IdMejorador
+		return 0
+	END TRY
+	BEGIN CATCH
+		return -1
+	END CATCH
+END
