d1daebc3b248953622154242f3b799b02b19aa57
Juan1605 <juanda_2007@hotmail.es>
2022-08-11 20:47:03 -0300

    script admin

---
+++ ProyectoFinal/ProyectoFinal/2_Dominio/Entidades/ModuloEnum.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace ProyectoFinal._2_Dominio.Entidades
+{
+    public class ModuloEnum
+    {
+    }
+}
