661d2d9feb8d862cfd6d0c99cfb318085595ae6e
Juan1605 <juanda_2007@hotmail.es>
2022-08-17 20:38:34 -0300

    cambios primary key

--- ProyectoFinal/BancoDeGermoplasma/Tablas/Taxonomias.sql
+++ ProyectoFinal/BancoDeGermoplasma/Tablas/Taxonomias.sql
@@ -1,6 +1,7 @@
 ﻿CREATE TABLE [dbo].[Taxonomias]
 (
 	[idTaxonomia] INT IDENTITY (1, 1) NOT NULL, 
-    [nombre] NCHAR(10) NOT NULL, 
-    [activo] BIT NULL DEFAULT 1,
+    [nombre] NCHAR(10) NOT NULL UNIQUE, 
+    [activo] BIT NULL DEFAULT 1, 
+    CONSTRAINT [PK_Taxonomias] PRIMARY KEY ([idTaxonomia]),
 )
