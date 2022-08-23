dd05b8fc752c90ab8d6db874bc133df1db4cc975
Juan1605 <juanda_2007@hotmail.es>
2022-08-22 22:58:11 -0300

    abm sobre

---
+++ ProyectoFinal/BancoDeGermoplasma/StoreProcedure/AltaSobre.sql
@@ -0,0 +1,18 @@
+﻿CREATE PROCEDURE [dbo].[AltaSobre] 
+	@Ubicacion varchar(50),
+	@Humedad int,
+	@Germinacion int,
+	@IdLote int,
+	@IdSemilla int,
+	@IdEstado int
+AS
+BEGIN
+	BEGIN TRY
+		INSERT INTO Sobres(ubicacion, humedad, germinacion, idLote, idSemilla, idEstado) 
+		VALUES (@Ubicacion, @Humedad, @Germinacion, @IdLote, @IdSemilla, @IdEstado)
+		return 0
+	END TRY
+	BEGIN CATCH
+		return -1
+	END CATCH
+END
