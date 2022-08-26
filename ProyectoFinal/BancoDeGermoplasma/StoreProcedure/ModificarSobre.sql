587c592f9c685971b9defc34a0efd5613268c3dc
Juan1605 <juanda_2007@hotmail.es>
2022-08-26 20:40:40 -0300

    cambios vigor

--- ProyectoFinal/BancoDeGermoplasma/StoreProcedure/ModificarSobre.sql
+++ ProyectoFinal/BancoDeGermoplasma/StoreProcedure/ModificarSobre.sql
@@ -3,6 +3,7 @@
 	@Ubicacion varchar(50),
 	@Humedad int,
 	@Germinacion int,
+	@Vigor int,
 	@IdLote int,
 	@IdSemilla int,
 	@IdEstado int
@@ -12,16 +13,17 @@ BEGIN
 		UPDATE Sobres SET 
 		ubicacion = @Ubicacion,
 		humedad = @Humedad,
-		germinacion = @Germinacion, 
+		germinacion = @Germinacion,
+		vigor = @Vigor, 
 		idLote = @IdLote, 
 		idSemilla = @IdSemilla, 
 		idEstado = @IdEstado 
 		WHERE idSobre = @IdSobre
-
-		IF @Humedad IS NOT NULL OR @Germinacion IS NOT NULL
+		IF @Humedad IS NOT NULL OR @Germinacion IS NOT NULL OR @Vigor IS NOT NULL
 			UPDATE Sobres SET
 			humedad = @Humedad,
-			germinacion = @Germinacion
+			germinacion = @Germinacion,
+			vigor = @Vigor
 			WHERE idSemilla = @IdSemilla
 		RETURN 0
 	END TRY
