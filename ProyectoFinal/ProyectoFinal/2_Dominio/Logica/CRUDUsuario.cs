62f1aeb0794457a5553d08e9285f16027b2416e9
Juan1605 <juanda_2007@hotmail.es>
2022-09-07 21:00:31 -0300

    hash y contraseña autogenerada

--- ProyectoFinal/ProyectoFinal/2_Dominio/Logica/CRUDUsuario.cs
+++ ProyectoFinal/ProyectoFinal/2_Dominio/Logica/CRUDUsuario.cs
@@ -4,7 +4,10 @@ using ProyectoFinal.Parametros.Salida;
 using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Security.Cryptography;
+using System.Text;
 using System.Web;
+using System.Web.Configuration;
 
 namespace ProyectoFinal._2_Dominio.Logica
 {
@@ -12,11 +15,13 @@ namespace ProyectoFinal._2_Dominio.Logica
     {
         public AltaUsuarioOut AltaUsuario(AltaUsuarioIn input)
         {
+            var contrasena = GenerarContrasena();
+            input.Usuario.Contrasena = ComputeSha256Hash(contrasena);
             var resultado = new PUsuario().AltaUsuario(input);
             if (resultado.Status.StatusCode == 200) {
 
                 Mail mail = new Mail();
-                mail.Enviar(input.Usuario.Mail, input.Usuario.Nombre, input.Usuario.Contrasena);           
+                mail.Enviar(input.Usuario.Mail, input.Usuario.Nombre, contrasena);           
             }
             return resultado;
 
@@ -48,6 +53,40 @@ namespace ProyectoFinal._2_Dominio.Logica
         public ListarUsuariosOut ListarUsuarios(ListarUsuariosIn input)
         {
             return new PUsuario().ListarUsuarios(input);
+        }
+
+        private string GenerarContrasena()
+        {
+            Random rdn = new Random();
+            string caracteres = WebConfigurationManager.AppSettings["Caracteres"];
+            int longitud = caracteres.Length;
+            char letra;
+            int longitudContrasena = int.Parse(WebConfigurationManager.AppSettings["LargoContrasena"]);
+            string contrasenaAleatoria = string.Empty;
+            for (int i = 0; i < longitudContrasena; i++)
+            {
+                letra = caracteres[rdn.Next(longitud)];
+                contrasenaAleatoria += letra.ToString();
+            }
+            return contrasenaAleatoria;
+        }
+
+        private static string ComputeSha256Hash(string rawData)
+        {
+            // Create a SHA256   
+            using (SHA256 sha256Hash = SHA256.Create())
+            {
+                // ComputeHash - returns byte array  
+                byte[] bytes = sha256Hash.ComputeHash(Encoding.UTF8.GetBytes(rawData));
+
+                // Convert byte array to a string   
+                StringBuilder builder = new StringBuilder();
+                for (int i = 0; i < bytes.Length; i++)
+                {
+                    builder.Append(bytes[i].ToString("x2"));
+                }
+                return builder.ToString();
+            }
         }
     }
 }
--- ProyectoFinal/ProyectoFinal/2_Dominio/Logica/Mail.cs
+++ ProyectoFinal/ProyectoFinal/2_Dominio/Logica/Mail.cs
@@ -1,6 +1,8 @@
 ﻿using SendGrid;
 using SendGrid.Helpers.Mail;
 using System;
+using System.Security.Cryptography;
+using System.Text;
 using System.Threading.Tasks;
 using System.Web.Configuration;
 
@@ -16,7 +18,7 @@ namespace ProyectoFinal._2_Dominio.Logica
             var subject = WebConfigurationManager.AppSettings["Asunto"];
             var to = new EmailAddress(email, usuario);
             var plainTextContent = contrasena;
-            var htmlContent = "<strong>"+contrasena+"</strong>";
+            var htmlContent = "<strong>" + contrasena + "</strong>";
             var msg = MailHelper.CreateSingleEmail(from, to, subject, plainTextContent, htmlContent);
             var response = client.SendEmailAsync(msg);
         }
