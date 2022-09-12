f9a546d0b3b42a4cba7e9974a5a8b232c2b8423b
FedeRivers <fedeede323@hotmail.com>
2022-09-11 23:17:52 -0300

    Se agregan estadísticas y se actualiza a Angular 12

--- Interfaz/ProyectoFinal/src/app/Components/Estadistica/estadistica.component.ts
+++ Interfaz/ProyectoFinal/src/app/Components/Estadistica/estadistica.component.ts
@@ -6,10 +6,52 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./estadistica.component.css']
 })
 export class EstadisticaComponent implements OnInit {
+  single = [
+    {
+      "name": "Germany",
+      "value": 8940000
+    },
+    {
+      "name": "USA",
+      "value": 5000000
+    },
+    {
+      "name": "France",
+      "value": 7200000
+    },
+      {
+      "name": "UK",
+      "value": 6200000
+    }
+  ];
+  view: [number, number] = [700, 400];
 
-  constructor() { }
+  // options
+  gradient: boolean = true;
+  showLegend: boolean = true;
+  showLabels: boolean = true;
+  isDoughnut: boolean = false;
 
+  colorScheme = {
+    domain: ['#5AA454', '#A10A28', '#C7B42C', '#AAAAAA']
+  };
+
+  constructor() {
+    //Object.assign(this, { single });
+  }
   ngOnInit(): void {
+
   }
 
+  onSelect(data:any): void {
+    console.log('Item clicked', JSON.parse(JSON.stringify(data)));
+  }
+
+  onActivate(data:any): void {
+    console.log('Activate', JSON.parse(JSON.stringify(data)));
+  }
+
+  onDeactivate(data:any): void {
+    console.log('Deactivate', JSON.parse(JSON.stringify(data)));
+  }
 }
